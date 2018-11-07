85e90b8cadfacae5358390dd040fea082df9c24b
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:22:35 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{industrial_dual_analog_in_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_dual_analog_in_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Analog In Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Analog In Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_dual_analog_in_v2_bricklet = IndustrialDualAnalogInV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let idai = IndustrialDualAnalogInV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for voltage events.
-    let voltage_listener = industrial_dual_analog_in_v2_bricklet.get_voltage_receiver();
-    // Spawn thread to handle received events. This thread ends when the industrial_dual_analog_in_v2_bricklet
+    // Create receiver for voltage events.
+    let voltage_receiver = idai.get_voltage_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `idai` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in voltage_listener {
-            println!("Channel: {}", event.channel);
-            println!("Voltage: {}{}", event.voltage as f32 / 1000.0, " V");
+        for voltage in voltage_receiver {
+            println!("Channel: {}", voltage.channel);
+            println!("Voltage: {} V", voltage.voltage as f32 / 1000.0);
             println!();
         }
     });
 
-    // Set period for voltage (channel 0) callback to 1s (1000ms) without a threshold
-    industrial_dual_analog_in_v2_bricklet.set_voltage_callback_configuration(0, 1000, false, 'x', 0, 0);
+    // Set period for voltage (channel 0) callback to 1s (1000ms) without a threshold.
+    idai.set_voltage_callback_configuration(0, 1000, false, 'x', 0, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_dual_analog_in_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_dual_analog_in_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Analog In Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual Analog In Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_dual_analog_in_v2_bricklet = IndustrialDualAnalogInV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let idai = IndustrialDualAnalogInV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current voltage from channel 0
-    let voltage = industrial_dual_analog_in_v2_bricklet.get_voltage(0).recv()?;
-    println!("Voltage (Channel 0): {}{}", voltage as f32 / 1000.0, " V");
+    // Get current voltage from channel 0.
+    let voltage = idai.get_voltage(0).recv()?;
+    println!("Voltage (Channel 0): {} V", voltage as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
