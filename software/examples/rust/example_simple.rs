82d5654a46d1e809dc500f917c4d32477e46d184
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:54:45 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,8 +1,6 @@
-use std::{io, error::Error};
+use std::{error::Error, io};
 
-use tinkerforge::{ip_connection::IpConnection, 
-                  industrial_dual_analog_in_v2_bricklet::*};
-
+use tinkerforge::{industrial_dual_analog_in_v2_bricklet::*, ip_connection::IpConnection};
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -13,11 +11,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     let idai = IndustrialDualAnalogInV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-    // Don't use device before ipcon is connected.
+                                          // Don't use device before ipcon is connected.
 
-		// Get current voltage from channel 0.
-let voltage = idai.get_voltage(0).recv()?;
-		println!("Voltage (Channel 0): {} V", voltage as f32 /1000.0);
+    // Get current voltage from channel 0.
+    let voltage = idai.get_voltage(0).recv()?;
+    println!("Voltage (Channel 0): {} V", voltage as f32 / 1000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
