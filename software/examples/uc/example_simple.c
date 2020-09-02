21b9e3509d468ea1eb6d929671e62542d484f3a0
Erik Fleckstein <erik@tinkerforge.com>
2020-09-02 09:55:25 +0200

    uc: Add example driver note.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,3 +1,7 @@
+// This example is not self-contained.
+// It requres usage of the example driver specific to your platform.
+// See the HAL documentation.
+
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_industrial_dual_analog_in_v2.h"
 
